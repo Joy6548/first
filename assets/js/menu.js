739fbbd7aa0a986a0a848ee8d3abbfb670ab8b2b
Joy <jjigijoy@naver.com>
2023-03-17 13:51:01 +0900

    first commit

---
+++ assets/js/menu.js
@@ -0,0 +1,11 @@
+// $(function (){
+//   $('.header__menu__ham').on('click',function(){
+//     $('.header__menu__ham').toggleClass('active');
+//     $('.header__menu__close').toggleClass('close');
+//   });
+//   $('.header__menu__close').on('click',function(){
+//     $('.header__menu__ham').toggleClass('active');
+//     $('.header__menu__close').toggleClass('close');
+//   });
+// });
+
